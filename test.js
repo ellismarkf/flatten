7419f12338f6382a58e41d603e0b726de3943021
Mark F. Ellis II <ellismarkf@gmail.com>
2016-06-27 12:11:53 -0500

    add more test expectations

--- test.js
+++ test.js
@@ -2,10 +2,25 @@ var expect = require('chai').expect;
 var flatten = require('./flatten.js');
 
 describe('flatten', function() {
-	it('should flatten an array of arbitrarily nested arrays of integers into a flat array of integers', function() {
+	xit('should flatten an array of arbitrarily nested arrays of integers into a flat array of integers', function() {
+		var nestedArray = [[1,2,[3]],4];
+		var expectedResult = [1, 2, 3, 4];
+		var flattenedArray = flatten(nestedArray);
+
+		expect(flattenedArray.length).to.equal(4);
+		expect(flattenedArray).to.eql(expectedResult);
+	});
+	it('should return an array with length property equal to the number of integers in nested array', function() {
 		var nestedArray = [[1,2,[3]],4];
 		var flattenedArray = flatten(nestedArray);
 
 		expect(flattenedArray.length).to.equal(4);
+	});
+	it('should return an array containing all elements of nested array', function() {
+		var nestedArray = [[1,2,[3]],4];
+		var expectedResult = [1, 2, 3, 4];
+		var flattenedArray = flatten(nestedArray);
+
+		expect(flattenedArray).to.eql(expectedResult);
 	});
 });
