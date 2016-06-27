35c69c293ea86ee909514825de095fdd391b49b2
Mark F. Ellis II <ellismarkf@gmail.com>
2016-06-27 12:50:16 -0500

    add working solution and harder test expectation

--- flatten.js
+++ flatten.js
@@ -1,5 +1,11 @@
 function flatten(array) {
-	return array
+	return array.reduce( function(result, element) {
+		if (element.length) {
+			return result.concat(flatten(element));
+		} else {
+			return result.concat(element);
+		}
+	}, []);
 }
 
 module.exports = flatten;
--- test.js
+++ test.js
@@ -2,14 +2,6 @@ var expect = require('chai').expect;
 var flatten = require('./flatten.js');
 
 describe('flatten', function() {
-	xit('should flatten an array of arbitrarily nested arrays of integers into a flat array of integers', function() {
-		var nestedArray = [[1,2,[3]],4];
-		var expectedResult = [1, 2, 3, 4];
-		var flattenedArray = flatten(nestedArray);
-
-		expect(flattenedArray.length).to.equal(4);
-		expect(flattenedArray).to.eql(expectedResult);
-	});
 	it('should return an array with length property equal to the number of integers in nested array', function() {
 		var nestedArray = [[1,2,[3]],4];
 		var flattenedArray = flatten(nestedArray);
@@ -21,6 +13,14 @@ describe('flatten', function() {
 		var expectedResult = [1, 2, 3, 4];
 		var flattenedArray = flatten(nestedArray);
 
+		expect(flattenedArray).to.eql(expectedResult);
+	});
+	it('should flatten an array of arbitrarily nested arrays of integers into a flat array of integers', function() {
+		var nestedArray = [[[1, 2], [3, [4, 5, 6, [7]]]], 8, [9, [10, [11]]]];
+		var expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+		var flattenedArray = flatten(nestedArray);
+
+		expect(flattenedArray.length).to.equal(11);
 		expect(flattenedArray).to.eql(expectedResult);
 	});
 });
